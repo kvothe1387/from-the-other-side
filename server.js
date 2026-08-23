e5dce4ac47d04dc2a3b40ef68999c844250ed4ed
David McCullough <dmccullough488@gmail.com>
2026-08-23 06:38:03 -0500

    serve the frontend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
 
-  await serveStatic(req, __dirname, res)
+  await serveStatic(req, res, __dirname)
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
