af064b689acab8888a47778887bbe16c326ccaf9
David McCullough <dmccullough488@gmail.com>
2026-08-23 05:36:33 -0500

    serve index.html
    created exp fun sendRes

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ import { serveStatic } from './utils/serveStatic.js'
 const PORT = 8000
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
-  serveStatic(__dirname)
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<html><h1>The server is working</h1></html>')
+  await serveStatic(req, __dirname, res)
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,6 +1,14 @@
 import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
 
-export function serveStatic(baseDir) {
+export async function serveStatic(baseDir, res) {
   const filePath = path.join(baseDir, 'public', 'index.html')
-  console.log(filePath)
+
+  try {
+    const content = await fs.readFile(filePath)
+    sendResponse(res, 200, 'text/html', content)
+  } catch (error) {
+    console.log(error)
+  }
 }
